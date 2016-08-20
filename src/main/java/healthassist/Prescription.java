0fba130387b899aca8255629986e0dda38e2109e
Naveen Singh <naveen.singh@leanpitch.com>
2016-08-20 22:45:28 +0530

    Added Main classes

---
+++ src/main/java/healthassist/Prescription.java
@@ -0,0 +1,54 @@
+package healthassist;
+
+public class Prescription {
+	String patient, doctor, visitDate, vitisTime, fileName, reason;
+
+	public String getPatient() {
+		return patient;
+	}
+
+	public void setPatient(String patinet) {
+		this.patient = patinet;
+	}
+
+	public String getDoctor() {
+		return doctor;
+	}
+
+	public void setDoctor(String doctor) {
+		this.doctor = doctor;
+	}
+
+	public String getVisitDate() {
+		return visitDate;
+	}
+
+	public void setVisitDate(String visitDate) {
+		this.visitDate = visitDate;
+	}
+
+	public String getVitisTime() {
+		return vitisTime;
+	}
+
+	public void setVitisTime(String vitisTime) {
+		this.vitisTime = vitisTime;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public String getReason() {
+		return reason;
+	}
+
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+
+}
---
+++ src/main/java/healthassist/PrescriptionBLL.java
@@ -0,0 +1,11 @@
+package healthassist;
+
+public class PrescriptionBLL {
+
+	public String savePrescription(Prescription prescription) {
+		// TODO Auto-generated method stub
+		//return null;
+		return "Prescription got added.";
+	}
+
+}
