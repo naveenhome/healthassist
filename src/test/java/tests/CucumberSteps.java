67f6cb372083183d439156afbffdbc9ad7cb1bf7
Naveen Singh <naveen.singh@leanpitch.com>
2016-08-20 21:22:42 +0530

    1st commit on Ubuntu

---
+++ src/test/java/tests/CucumberSteps.java
@@ -0,0 +1,33 @@
+package tests;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class CucumberSteps {
+	@Given("^user enter patient as \"([^\"]*)\" doctor as \"([^\"]*)\" visited on \"([^\"]*)\" at \"([^\"]*)\" and file name \"([^\"]*)\" for \"([^\"]*)\"$")
+	public void user_enter_patient_as_doctor_as_visited_on_at_and_file_name_for(String arg1, String arg2, String arg3, String arg4, String arg5, String arg6) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^click on \"([^\"]*)\"$")
+	public void click_on(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^message get displayed \"([^\"]*)\"$")
+	public void message_get_displayed(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^Also display \"([^\"]*)\" button$")
+	public void also_display_button(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+}
