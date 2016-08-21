73958d9bb0a8c282b9b4c9092a3bd3219e028c42
Naveen Singh <naveen.singh@leanpitch.com>
2016-08-21 10:34:46 +0530

    Test Changes

--- src/test/java/tests/CucumberSteps.java
+++ src/test/java/tests/CucumberSteps.java
@@ -4,30 +4,44 @@ import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import healthassist.Prescription;
+import healthassist.PrescriptionBLL;
+import junit.framework.TestCase;
 
-public class CucumberSteps {
+public class CucumberSteps extends TestCase {
+	Prescription prescription; 
 	@Given("^user enter patient as \"([^\"]*)\" doctor as \"([^\"]*)\" visited on \"([^\"]*)\" at \"([^\"]*)\" and file name \"([^\"]*)\" for \"([^\"]*)\"$")
-	public void user_enter_patient_as_doctor_as_visited_on_at_and_file_name_for(String arg1, String arg2, String arg3, String arg4, String arg5, String arg6) throws Throwable {
+	public void user_enter_patient_as_doctor_as_visited_on_at_and_file_name_for(String patient, String doctor, String visitDate, String visitTime, String fileName, String reason) throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	    //throw new PendingException();
+		prescription = new Prescription();
+		prescription.setPatient(patient);
+		prescription.setDoctor(doctor);
+		prescription.setVisitDate(visitDate);
+		prescription.setVitisTime(visitTime);
+		prescription.setFileName(fileName);
+		prescription.setReason(reason);
+		
 	}
 
 	@When("^click on \"([^\"]*)\"$")
 	public void click_on(String arg1) throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	    //throw new PendingException();
 	}
 
 	@Then("^message get displayed \"([^\"]*)\"$")
 	public void message_get_displayed(String arg1) throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	    //throw new PendingException();
+		PrescriptionBLL obj = new PrescriptionBLL();
+		assertEquals(arg1, obj.savePrescription(prescription));
 	}
 
 	@Then("^Also display \"([^\"]*)\" button$")
 	public void also_display_button(String arg1) throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	   // throw new PendingException();
 	}
 
 }
